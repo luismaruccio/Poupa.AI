7810ba78aab56203d75770c9f49f2f192088e149
Luis Guilherme Nascimento Maruccio <luis.maruccio@gmail.com>
2024-11-01 15:42:05 -0300

    Add project files.

---
+++ src/Poupa.AI.API/Program.cs
@@ -0,0 +1,24 @@
+using Poupa.AI.IoC.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDependencies();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ src/Poupa.AI.IoC/DependencyInjection/DependencyInjectionConfig.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Poupa.AI.IoC.DependencyInjection
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection AddDependencies(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
